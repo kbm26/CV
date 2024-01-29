4f5a7873cc74714a4fabb50b8a83690119cedd13
kylebmoodley <kylebmoodley@gmail.com>
2024-01-29 15:16:44 +0200

    store to a database

--- Js/main.js
+++ Js/main.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const togglebtn = document.querySelector('.toggle_btn')
     const menu = document.querySelector('.menu')
 
     togglebtn.addEventListener('click',function(){
         menu.classList.toggle('open')
     })
-});
+
+    const form = document.querySelector('.form')
+    form.addEventListener("submit", (e)=>{
+    e.preventDefault
+    const name = document.getElementById('fullName').value
+    const email = document.getElementById('email').value
+    const message = document.getElementById('email').value
+    alert(name,email,message)
+})
+})
+
