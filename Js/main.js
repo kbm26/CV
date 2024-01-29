73190dd00a42edbc2a7bd3d72b5b8a339b4d4901
kylebmoodley <kylebmoodley@gmail.com>
2024-01-29 10:25:25 +0200

    removed profile picture

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Select the elements
     const togglebtn = document.querySelector('.toggle_btn')
     const menu = document.querySelector('.menu')
 
-    // Add click event listener to the toggle button
     togglebtn.addEventListener('click',function(){
         menu.classList.toggle('open')
     })
